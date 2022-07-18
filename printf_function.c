a533b63dcbba536ad46477373f1759efb2b4611c
Ansuman Das <emailofansumandas@gmail.com>
2022-07-18 23:47:53 +0530

    c file regarding printf function

---
+++ printf_function.c
@@ -0,0 +1,7 @@
+//How printf() works? Dt: 18/07/2022 ,Kendrapara.
+#include<stdio.h>
+int main()
+{
+    printf("Hellow Ansuman\n"); /* The printf() is used to give us output i.e anything we write in printf function this simply prints that irrespective of capital or small letters. As we know C is a case sensitive language printf should be always written in small letters. "\n"(Backslash n) is used for print the output in a new line */
+    return 0;
+}
